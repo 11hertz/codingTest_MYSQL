cd53a24948ce84b1725919746e056a2f24714fc4
KIHOON LEE <reachzin@naver.com>
2024-05-09 01:14:26 +0900

    [level 2] Title: 올바른 괄호, Time: 9.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -3,7 +3,7 @@ def solution(s):
     stack = []
     for x in s:
         if x == '(' : stack.append(x)
-        elif stack : stack.pop()
+        elif len(stack) > 0 : stack.pop()
         else : return False
     
     return True if len(stack) == 0 else False
